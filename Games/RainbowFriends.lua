10c807e71db00b8d05f60feceba29129da92a4dc
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-03-14 21:15:38 -0400

    Update RainbowFriends.lua

--- Games/RainbowFriends.lua
+++ Games/RainbowFriends.lua
@@ -5,6 +5,8 @@ local GameName = "RainbowFriends"
 local TabName = "Main"
 local RunService = game:GetService("RunService")
 getgenv().AutoWin = false
+getgenv().MonsterESP = false
+getgenv().ItemESP = false
 getgenv().antiAFK = false
 getgenv().FullBright = false
 getgenv().NoClip = false
@@ -65,20 +67,76 @@ local AutoWinToggle = Tab:CreateToggle({
                             firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, child.TouchTrigger, 1)
                             wait()
                             firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, child.TouchTrigger, 0)
+                            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GroupBuildStructures:GetChildren()[1].Trigger.CFrame
                         else
                             firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, child, 1)
                             wait()
                             firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, child, 0)
+                            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GroupBuildStructures:GetChildren()[1].Trigger.CFrame
                         end
                     end
                 end
-                wait(0.5)
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GroupBuildStructures:GetChildren()[1].Trigger.CFrame
             end
         end)
     end,
 })
 
+---> ESP Section <---
+local Label = Tab:CreateLabel("ESP")
+
+local MESPToggle = Tab:CreateToggle({
+    Name = "Monster ESP",
+    CurrentValue = false,
+    Flag = "MESP",
+    Callback = function(Value)
+        MonsterESP = Value
+        spawn(function()
+            while MonsterESP do
+                wait()
+                for _, child in pairs(workspace.Monsters:GetChildren()) do
+                    if not child:FindFirstChild("ESP") then
+                        local ESP = Instance.new("Highlight")
+                        ESP.Name = "ESP"
+                        ESP.Parent = child
+                    end
+                end
+            end
+            for _, child in pairs(workspace.Monsters:GetChildren()) do
+                if child:FindFirstChild("ESP") then
+                    child:FindFirstChild("ESP"):Destroy()
+                end
+            end
+        end)
+    end,
+})
+
+local IESPToggle = Tab:CreateToggle({
+    Name = "Item ESP",
+    CurrentValue = false,
+    Flag = "IESP",
+    Callback = function(Value)
+        ItemESP = Value
+        spawn(function()
+            while ItemESP do
+                wait()
+                for _, child in pairs(workspace:GetChildren()) do
+                    if child.Name:match("Block") or child.Name:match("Food") or child.Name:match("Fuse") or child.Name:match("Battery") and not child:FindFirstChild("ESP") then
+                        local ESP = Instance.new("Highlight")
+                        ESP.Name = "ESP"
+                        ESP.Parent = child
+                        ESP.FillColor = Color3.new(0, 1, 0)
+                    end
+                end
+            end
+            for _, child in pairs(workspace.Monsters:GetChildren()) do
+                if child:FindFirstChild("ESP") and child.Name:match("Block") or child.Name:match("Food") or child.Name:match("Fuse") or child.Name:match("Battery") then
+                    child:FindFirstChild("ESP"):Destroy()
+                end
+            end
+        end)
+    end,
+})
 
 ---> Misc tab <---
 
