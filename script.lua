97f0dc4a0651c7b852f0fc3a2ba52c8cfba12a60
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-03-14 20:39:15 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 11063612131 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/1Jump.lua"))()
+elseif game.PlaceId == 7991339063 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/1Jump.lua"))()
 end
