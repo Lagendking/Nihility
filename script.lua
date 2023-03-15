fb606c15183c60f6336cf76fd39f965deddac123
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-03-14 20:42:22 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 11063612131 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/1Jump.lua"))()
-elseif game.PlaceId == 7991339063 then
+elseif game.PlaceId == 8888615802 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/RainbowFriends.lua"))()
 end
