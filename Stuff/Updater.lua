1d58031587c3ebc7a4ec61946d940e7579a7f9d1
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-04-18 15:52:27 -0400

    Update Updater.lua

--- Stuff/Updater.lua
+++ Stuff/Updater.lua
@@ -1,4 +1,5 @@
 if (writefile) then
+    print("Hi")
     writefile("NihilityHub/Games/RainbowFriends.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Stuff/Main.lua"))
     appendfile("NihilityHub/Games/RainbowFriends.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/RainbowFriends.lua"))
 end
