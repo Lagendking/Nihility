d07c571166c47a761208d76242f0404041c0d278
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-04-18 22:11:18 -0400

    Update Updater.lua

--- Stuff/Updater.lua
+++ Stuff/Updater.lua
@@ -1,5 +1,11 @@
 if (writefile) then
-    print("Hi")
+    print("Welcome To Nihility Hub, ".. game.Players.LocalPlayer.Name.. ".")
+    
+    ---> Rainbow Friends <---
     writefile("NihilityHub/Games/RainbowFriends.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Stuff/Main.lua"))
     appendfile("NihilityHub/Games/RainbowFriends.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/RainbowFriends.lua"))
+    
+    ---> +1 jump <---
+    writefile("NihilityHub/Games/1jump.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Stuff/Main.lua"))
+    appendfile("NihilityHub/Games/1jump.txt", game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/1Jump.lua"))
 end
