282fd4ddc3eb594fc3eebf6178f32a7c520a549c
Lagend <105041466+Lagendking@users.noreply.github.com>
2023-04-19 00:39:04 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,6 +8,8 @@ elseif game.PlaceId == 3276265788 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/LOS/Desert.lua"))()
 elseif game.PlaceId == 7584496019 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/Nihility/main/Games/99%25.lua"))()
+elseif game.PlaceId == 12114398020 then
+    loadfile("NihilityHub/Games/1Point.txt")()
 else
     loadfile("NihilityHub/Main.txt")()
 end
